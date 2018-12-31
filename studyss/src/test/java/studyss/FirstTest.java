580302645108f559f0c37dcf0ff8461873c8ee5e
Botanys <Pogrebnuy.anton@gmail.com>
2018-12-31 11:19:56 +0200

    Changes

--- studyss/src/test/java/studyss/FirstTest.java
+++ studyss/src/test/java/studyss/FirstTest.java
@@ -3,13 +3,16 @@ package studyss;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import javax.lang.model.element.Element;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
@@ -24,7 +27,7 @@ public void launchBrowser(){
     driver = new ChromeDriver();
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     driver.manage().window().fullscreen();
-
+    driver.get("https://www.i.ua/");
 
 }
 
@@ -34,13 +37,7 @@ public void launchBrowser(){
    }
 
     @Test
-    public void testFirst() throws InterruptedException {
-
-    driver.get("https://www.i.ua/");
-
-
-
-
+    public void testFirst(){
 
     driver.findElement(By.xpath("//input[@name='login']"));
     driver.findElement(By.xpath("//input[@name='login']")).clear();driver.findElement(By.xpath("//input[@name='login']")).sendKeys("Meeet@ua.fm");
@@ -52,15 +49,11 @@ public void launchBrowser(){
 
     Select chouseDomain = new Select(driver.findElement(By.xpath("//select[@name='domn']")));
     chouseDomain.selectByValue("ua.fm");
+
     driver.findElement(By.xpath("//input[@value='Войти']")).click();
 
-    
-
-
-
-    Thread.sleep(10000);
-
-
+    driver.findElement(By.xpath("//div[@class][1]/span[@class]/input[@type='checkbox']")).click();
+    driver.findElement(By.xpath("//div[@class][2]/span[@class]/input[@type='checkbox']")).click();
 
     }
 
