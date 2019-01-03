bb5f5f5112b97fb0ee073a286092e3a1c23fc50a
Botanys <Pogrebnuy.anton@gmail.com>
2019-01-03 16:56:11 +0200

    Changes

--- studyss/src/test/java/studyss/appmanager/ApplicationManager.java
+++ studyss/src/test/java/studyss/appmanager/ApplicationManager.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import studyss.Model.MailFillForm;
+
 
 import java.util.concurrent.TimeUnit;
 
--- studyss/src/test/java/studyss/tests/SecondTest.java
+++ studyss/src/test/java/studyss/tests/SecondTest.java
@@ -23,10 +23,3 @@ public class SecondTest extends TestBase{
 }
 
 
-
-
-
-
-
-
-
