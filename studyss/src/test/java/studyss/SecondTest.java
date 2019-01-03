ea9986b0edde35814ff7ea2b1b9c71e7b38bb19b
Botanys <Pogrebnuy.anton@gmail.com>
2019-01-03 09:48:02 +0200

    Update SecondTest.java

--- studyss/src/test/java/studyss/SecondTest.java
+++ studyss/src/test/java/studyss/SecondTest.java
@@ -2,8 +2,6 @@ package studyss;
 
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 
@@ -16,7 +14,6 @@ public class SecondTest extends TestBase{
     @Test
 
     public void sendEmptyEmail() throws Exception {
-
         login("Meeet@ua.fm", "123456qwerty", "ua.fm");
         clickCreateLetterHeaderMainMailBox();
         clickSendButtonTopCreateNewLetter();
